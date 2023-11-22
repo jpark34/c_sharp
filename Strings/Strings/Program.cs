bbc27a310b7402ad1836f2c2fcf7cd1d5dfb4266
Jarrett Parker <jarrettparker34@gmail.com>
2023-11-21 21:54:15 -0500

    Started Strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+string firstFriend = "Maria";
+string secondFriend = "Scott";
+
+// Two ways of writing the same thing
+// Top being string interpolation
+// Bottom being composite formatting
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+Console.WriteLine("My friend is " + firstFriend + " and " + secondFriend);
+
+// Trim() functions that allow you to remove whitespace
+string trimFriend = "     Maria     ";
+string trimFront = firstFriend.TrimStart();
+string trimLast = firstFriend.TrimEnd();
+Console.WriteLine($"Trim extra characters {firstFriend.Trim()}");
