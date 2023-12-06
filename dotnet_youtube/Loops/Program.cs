e714ccdef869804677d9675e711a183354c3cb7d
Jarrett Parker <jarrettparker34@gmail.com>
2023-12-05 22:07:46 -0500

    Finished loops and lists

--- dotnet_youtube/Loops/Program.cs
+++ dotnet_youtube/Loops/Program.cs
@@ -1,4 +1,20 @@
 ﻿int counter = 0;
-counter++;
 
-Console.WriteLine(counter);
+// optimized for counting
+while (counter < 5) {
+    Console.WriteLine(counter);
+    counter++;
+}
+
+// do while loops will always run at least once
+do {
+    Console.WriteLine(counter);
+    counter++;
+} while (counter < 5);
+
+
+for (int i = 0; i < 5; i++) {
+    if (i == 3) {
+        Console.WriteLine(i);
+    }
+}
---
+++ dotnet_youtube/ManageData/Program.cs
@@ -0,0 +1,13 @@
+﻿// construct a new list object
+// lists are mutable
+var names = new List<string> { "<name>", "Ana", "Felipe" };
+
+names.Add("David");
+names.Add("Damian");
+names.Add("Maria");
+
+// run through everything that is contained in the list
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
